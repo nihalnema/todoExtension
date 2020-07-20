43410d46ff2d50f3f4ec62501b068958a80922d8
shweta4901 <shwetashinde897@gmail.com>
2020-07-20 16:11:30 +0530

    added support for notes page

--- src/popup/HomePage.tsx
+++ src/popup/HomePage.tsx
@@ -5,24 +5,12 @@ import "./HomePage.scss";
 
 // Componect class for Home page.
 class HomePage extends React.Component {
-    constructor(props){
-        super(props);
-        this.state={
-            isClicked:false
-        }
-    }
-
-    changeColor=()=>{
-        
-        this.setState({
-            isClicked: true
-        });
-    }
+   
   render() {
     return (
       <div className="homePage">
-        <div className="toDo" onClick = {this.changeColor}>ToDo</div>
-        <div className="toDo" onClick = {this.changeColor}>Bookmarks</div>
+        <div className="notes">Notes</div>
+        <div className="notes">Bookmarks</div>
       </div>
     );
   }
